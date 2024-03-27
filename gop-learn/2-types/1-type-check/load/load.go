22d059ab3dd498e374c3d0a9d2605b799c5b5d7f
luoliwoshang <2643523683@qq.com>
2024-03-27 16:36:03 +0800

    add type-check use

--- gop-learn/2-types/1-type-check/load/load.go
+++ gop-learn/2-types/1-type-check/load/load.go
@@ -11,6 +11,8 @@ import (
 func LoadPackage(fset *token.FileSet, name string, filenames ...string) (*types.Package, *types.Info, error) {
 	var files []*ast.File
 	for _, filename := range filenames {
+		// 解析每个源代码文件
+
 		f, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
 		if err != nil {
 			return nil, nil, err
@@ -18,8 +20,11 @@ func LoadPackage(fset *token.FileSet, name string, filenames ...string) (*types.
 		files = append(files, f)
 	}
 
+	// 创建类型检查器的配置
 	conf := &types.Config{}
 	conf.Importer = importer.Default()
+
+	// 创建一个新的包和类型检查的信息对象
 	info := &types.Info{
 		Types:      make(map[ast.Expr]types.TypeAndValue),
 		Defs:       make(map[*ast.Ident]types.Object),
@@ -29,6 +34,12 @@ func LoadPackage(fset *token.FileSet, name string, filenames ...string) (*types.
 		Scopes:     make(map[ast.Node]*types.Scope),
 	}
 	pkg := types.NewPackage("", name)
+
+	// 	types.NewChecker 是 go/types 包中的一个函数，用于创建一个类型检查器的实例。
+	// conf 是 types.Config 类型的对象，包含了类型检查器的配置信息。
+	// fset 是 *token.FileSet 类型的对象，用于跟踪源代码文件的位置信息。
+	// pkg 是 *types.Package 类型的对象，表示要进行类型检查的包。
+	// info 是 *types.Info 类型的对象，用于存储类型检查的结果信息。
 	check := types.NewChecker(conf, fset, pkg, info)
 	err := check.Files(files)
 	return pkg, info, err
---
+++ gop-learn/2-types/1-type-check/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"go/token"
+	"go/types"
+	"gop-learn/gop-learn/2-types/1-type-check/load"
+)
+
+func main() {
+	fset := token.NewFileSet()
+	pkg, info, err := load.LoadPackage(fset, "main", "./testdata/test.go")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(pkg)
+	fmt.Println(pkg.Scope())
+	fmt.Println(info)
+	// 在类型检查的结果中获取信息
+	for ident, obj := range info.Defs {
+		// 只检查变量定义
+		if v, ok := obj.(*types.Var); ok {
+			// 检查变量是否被使用
+			fmt.Printf("标识符 %s 的对象类型为: %s\n", ident.Name, v.Type())
+			name := ident.Name
+			used := false
+			// Uses中的标识符存储的位置是使用的位置，而Defs存储的位置是定义的位置，所以通过ident.name来判断
+			for _ident := range info.Uses {
+				if _ident.Name == name {
+					used = true
+				}
+			}
+			if !used {
+				fmt.Printf("Variable %s is unused\n", ident.Name)
+			}
+		}
+
+	}
+}
